dc89cb0553d9cbbf2338c14f23c08439e22cc1db
Olivier Lange <olange@petit-atelier.ch>
2019-03-19 13:36:09 +0100

    Minor improvements in two citations of Wendy Ginsberg and Rob Dodson (#5)

--- public/wcp-parts-D01.js
+++ public/wcp-parts-D01.js
@@ -2,12 +2,12 @@ import { html } from "lit-element";
 
 export function diapoDataBindingsMantra() {
   return html`
-    <h1>Vanilla data bindings</h1>
-    <blockquote class="big">
-      <p>« Data-in via attributes,<br/>
-        data-out via events »</p>
-    </blockquote>
-    <p><cite>— Rob Dodson, 30.11.2016, <a href="https://robdodson.me/interoperable-custom-elements/"
+    <h1>Dataflow — <em>Vanilla</em> data bindings</h1>
+    <blockquote class="big" cite="https://robdodson.me/interoperable-custom-elements/">
+      <p>« Keep this mantra in mind: ‹&nbsp;Data-in via attributes<br/>
+        and properties, data-out via events&nbsp;› »</p>
+      <p><cite>— Rob Dodson, 30.11.2016, <a href="https://robdodson.me/interoperable-custom-elements/"
       >Custom Elements That Work Anywhere</a></cite></p>
+    </blockquote>
   `;
 }
